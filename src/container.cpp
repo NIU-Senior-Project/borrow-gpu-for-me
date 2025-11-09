f5ecf26d01d4a7b617ebd1b93551301928c1fcaf
Young <young20050727@gmail.com>
2025-11-09 23:43:23 +0800

    feat: add launch_container()

--- src/container.cpp
+++ src/container.cpp
@@ -10,3 +10,15 @@ int pull_container(std::string container) {
 
     return 0; // Success
 }
+
+int launch_container(std::string container) {
+    std::string command = "docker run -d " + container + " > /dev/null 2>&1 \\ ";
+    // command += "--device /dev/kfd --device /dev/dri "; // AMD GPU support
+    command += "--gpus all "; // NVIDIA GPU support
+    int status = system(command.c_str());
+    if (WIFEXITED(status) && WEXITSTATUS(status) != 0) {
+        return -1; // Failed to launch container
+    }
+
+    return 0; // Success
+}
