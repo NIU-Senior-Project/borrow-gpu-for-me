940673e19cf75a0c901791d951ba8c4cf77112b3
Young <young20050727@gmail.com>
2026-03-01 02:23:50 +0800

    feat: add cuda example script

---
+++ batch/cuda.sh
@@ -0,0 +1,14 @@
+#!/bin/env bash
+
+apt update
+apt install -y cmake git
+
+git clone https://github.com/Young-TW/rocOdyssey.git
+
+cd rocOdyssey || exit
+
+cmake -B build -DODYSSEY_BACKEND=CUDA -DCMAKE_BUILD_TYPE=Release
+
+cmake --build build
+
+./build/exec
